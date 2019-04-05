c492b49b5cc664d962a21f7fc2448e28e7cd56ed
doctordre246 <47084275+doctordre246@users.noreply.github.com>
2019-04-05 19:30:39 -0400

    Update ListClient.cpp

--- ListClient.cpp
+++ ListClient.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "List.h"
-#include <vector>
+
 
 using namespace std;
 
@@ -8,13 +8,32 @@ int main()
 {
 List L1, L2;
 	L1.insert(1, 1);
-	L2.insert(2, 2);
+	L1.insert(2, 2);
 	L1.insert(3, 3);
-	L2.insert(4, 2);
+	L1.insert(4, 2);
 	L1.insert(5, 1);
  
  for (int i =0; i < L1.size(); i++){
-	cout << L1;}
+	cout << L1.size();
+}
+L1.clear();
+	for (int i =0; i < L1.size(); i++){
+		cout<<L1;
+	}
+L2.clear();
+	for (int i =0; i < L2.size(); i++){
+		cout<<L1.size();
+	}
+L1.size();	
+	for (int i =0; i < L1.size(); i++){
+		cout<<L1.size();
+	}
+L1.remove(1);
+L1.remove(3);
+	for (int i =0; i < L1.size(); i++){
+		cout<<L1;
+	}
+	
 	
 	// for (initialize ; comparison ; increment)
 	// { 	code 	}
@@ -23,14 +42,11 @@ List L1, L2;
 for (unsigned int i =0; i < v1.size(); i++){
 	cout << v1[i];
 }
-
 v1.pop_back();
 for (unsigned int i =0; i < v1.size(); i++){
 	cout << v1[i];
 }
-
 v1.clear();
-
 for (unsigned int i =0; i < v1.size(); i++){
 	cout << v1[i];
 }
@@ -38,3 +54,4 @@ for (unsigned int i =0; i < v1.size(); i++){
  //Do some stuff with L1, L2, ...
  // ...*/
 }
+
